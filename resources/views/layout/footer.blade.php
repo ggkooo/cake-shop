7f77b0d0c7caa4244487cf2e66f21e6fcc4e6aae
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-08-21 13:45:19 +0000

    fix: updated footer partial

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,10 @@
-<footer class="blog-footer">
-    <p>This website is built by <a href="https://github.com/mahinxander" target="_blank" rel="external">Md. Mahin Rahman</a> and <a href="https://github.com/ggkooo" target="_blank" rel="external">Giordano Bruno Biasi Berwig</a>.</p>
-    <p>
-        <a href="#">Back to top</a>
-    </p>
+<footer class="blog-footer text-center py-3">
+    <div class="container">
+        <p class="mb-0">
+            This website is built by 
+            <a href="https://github.com/mahinxander" target="_blank" rel="external">Md. Mahin Rahman</a> 
+            and 
+            <a href="https://github.com/ggkooo" target="_blank" rel="external">Giordano Bruno Biasi Berwig</a>.
+        </p>
+    </div>
 </footer>
