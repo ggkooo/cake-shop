c515440bef1bc96e84ef34e7fe0c0fab12b32bbd
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-08-21 13:23:59 +0000

    fix: updated scripts partial

--- resources/views/layout/scripts.blade.php
+++ resources/views/layout/scripts.blade.php
@@ -1,11 +1,1 @@
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
-<script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
-<script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/holder.min.js"></script>
-<script>
-    Holder.addTheme('thumb', {
-        bg: '#55595c',
-        fg: '#eceeef',
-        text: 'Thumbnail'
-    });
-</script>
+<script src="{{ asset('js/app.js') }}"></script>
